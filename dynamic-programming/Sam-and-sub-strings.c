8856d14e57bef92c6cd6981f5490db2ed5f3d43a
Taesueng Lee <taeseunglee159@gmail.com>
2018-04-20 12:01:40 +0900

    Solve Sam and sub string

--- dynamic-programming/Sam-and-sub-strings.c
+++ dynamic-programming/Sam-and-sub-strings.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define MOD 1000000007 // 10^9+7
+#define MAX_SIZE 200005
 
 /* global variables */
-int n;
+char num[MAX_SIZE];
 long long int res;
 
 void __init__(void);
 void __process__(void);
 void __print_out__(void);
+char* readline();
 
 int main()
 {
@@ -22,11 +26,19 @@ int main()
 
 void __init__(void)
 {
-  scanf("%d", &n);
+  fscanf(stdin, "%s", num);
 }
 
 void __process__(void)
 {
+  res = 0;
+  int len = strlen(num);
+  long long int weight = 1;
+  for (int n_i = len-1, freq = len; n_i > -1; --n_i, --freq)
+    {
+      res = (res + (num[n_i]-'0')*weight*freq) % MOD;
+      weight = (weight*10+1) % MOD;
+    }
 }
 
 void __print_out__(void)
